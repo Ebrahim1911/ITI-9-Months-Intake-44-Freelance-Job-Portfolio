a1b273c42fecbb0b09e94770258043351a17d3cf
“Ebrahim1911” <ebrahimn709@hmail.com>
2024-07-02 20:29:35 +0300

    Add Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,47 @@
-function Experience() {
-  return <div>Experience</div>;
+import "./Footer.css";
+import { FaGoogle } from "react-icons/fa";
+import { FiFigma } from "react-icons/fi";
+
+function Footer() {
+  return (
+    <footer>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer-socials">
+        <a href="mailto:aliyoumna440@gmail.com" target="_blank">
+          <FaGoogle />
+        </a>
+
+        <a
+          href="https://www.figma.com/files/team/1161394436801182066/user/1390004839210369985?fuid=1161394427456833982"
+          target="_blank"
+        >
+          <FiFigma />
+        </a>
+      </div>
+      <div className="footer-copyright">
+        <small>&copy;Youmna Ali Mohamed. All rights reserved. </small>
+      </div>
+    </footer>
+  );
 }
 
-export default Experience;
+export default Footer;
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,7 +1,56 @@
-import React from "react";
-
+import { BiCheck } from "react-icons/bi";
+import "./Services.css";
 function Services() {
-  return <div>Services</div>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className="container services-container">
+        <article className="service">
+          <div className="service-head">
+            <h3>UI && UX Design</h3>
+          </div>
+          <ul className="service-list">
+            <li>
+              <BiCheck className="service-list-icon" />
+              <p>Wireframing and Prototyping</p>
+            </li>
+            <li>
+              <BiCheck className="service-list-icon" />
+              <p>Interaction Design</p>
+            </li>
+            <li>
+              <BiCheck className="service-list-icon" />
+              <p>Accessibility Design</p>
+            </li>
+            <li>
+              <BiCheck className="service-list-icon" />
+              <p>Usability Testing</p>
+            </li>
+            <li>
+              <BiCheck className="service-list-icon" />
+              <p>Designing intuitive and visually appealing interfaces</p>
+            </li>
+            <li>
+              <BiCheck className="service-list-icon" />
+              <p>Crafting seamless user experiences</p>
+            </li>
+            <li>
+              <BiCheck className="service-list-icon" />
+              <p>
+                Visual Design Applying principles of typography, color theory,
+                and layout
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service-list-icon" />
+              <p>Design Systems and Documentation</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default Services;
