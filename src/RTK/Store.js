7385ab2f74bfe61a7eec4c36712c81ecd178e86e
“Ebrahim1911” <ebrahimn709@hmail.com>
2024-07-02 18:46:35 +0300

    Add state management⚔️

--- src/RTK/Store.js
+++ src/RTK/Store.js
@@ -1,1 +1,7 @@
-ECHO is on.
+import { configureStore } from "@reduxjs/toolkit";
+import NavSlice from "./slices/navSlice";
+export const store = configureStore({
+  reducer: {
+    nav: NavSlice,
+  },
+});
