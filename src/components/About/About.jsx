ac937f8301c4ed4e857ad7bc4e5132b7fdd4b870
Mahmoud Abdelaziz <mahmoudabdelazizibrahim11@gmail.com>
2024-07-02 20:07:29 +0300

    add contact and about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,49 @@
+// import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { VscFolderLibrary } from "react-icons/vsc";
+import "./About.css";
 function About() {
-  return <div>About</div>;
+  return (
+    <section id="about">
+      <h5>Go To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about-container">
+        <div className="about-me">
+          <div className="about-me-image">
+            <img src="" alt="mee" />
+          </div>
+        </div>
+        <div className="about-content">
+          <div className="about-cards">
+            <article className="about-card">
+              <FaAward className="about-icon" />
+              <h5>Experience</h5>
+              <small>Junior Level</small>
+            </article>
+            <article className="about-card">
+              <VscFolderLibrary className="about-icon" />
+              <h5>Projects</h5>
+              <small>8 Completed</small>
+            </article>
+          </div>
+          <p>
+            With a background in graphic design and a deep understanding of
+            human-computer interaction, I bring a unique blend of artistic
+            vision and technical expertise to every project. My journey in the
+            field of UI/UX design started 2 years ago, and since then, I’ve been
+            dedicated to honing my skills and expanding my knowledge. My design
+            philosophy is rooted in empathy, simplicity, and functionality. I
+            believe that great design is not just about aesthetics but also
+            about how effectively it solves users’ problems and enhances their
+            experience.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Lets Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,76 @@
+import "./Contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 function Contact() {
-  return <div>Contact</div>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_xntlm6s",
+      "template_phy1zrf",
+      form.current,
+      "fOFbZsDVEL0MMiuuc"
+    );
+    e.target.reset();
+  };
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact-container">
+        <div className="contact-options">
+          <article className="contact-option">
+            <MdOutlineEmail className="contact-option-icon" />
+            <h4>Email</h4>
+            <h5>aliyoumna440@gmail.com</h5>
+            <a href="mailto:aliyoumna440@gmail.com" target="_blank">
+              Send a Message
+            </a>
+          </article>
+          <article className="contact-option">
+            <RiMessengerLine className="contact-option-icon" />
+            <h4>Messenger</h4>
+            <h5>Youmna Ali mohamed</h5>
+            <a href="https://m.me/YouMnaAli" target="_blank">
+              Send a Message
+            </a>
+          </article>
+          <article className="contact-option">
+            <BsWhatsapp className="contact-option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>01090407468</h5>
+            <a href="https://wa.me/+201090407468" target="_blank">
+              Send a Message
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="text" name="name" placeholder="Your Email" required />
+          <input type="text" name="subject" placeholder="Subject..." required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
